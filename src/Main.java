181314fcbbcd572fa977dca5f957cebad489abfd
Kaan Gumusdag <uveyskaangumusdag@hotmail.com>
2021-10-12 17:18:57 +0300

    index

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+      double boy, kilo, index;
+      Scanner input = new Scanner(System.in);
+        System.out.print("Lütfen boyunuzu (metre cinsinde) giriniz : ");
+        boy = input.nextDouble();
+        System.out.print("Lütfen kilonuzu giriniz : ");
+        kilo = input.nextDouble();
+
+        index = (kilo / (boy * boy));
+        System.out.println("Vücut Kitle İndeksiniz : " + index);
+    }
+    }
